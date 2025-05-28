ff848df122b10963fa77ab072f6c7022008e0957
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-05-28 11:34:42 +0200

    feat: add DefatulLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DefaultLayout from './layouts/DefaultLayout';
 
 
 function App() {
@@ -9,21 +10,23 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/games' element={<div>Games</div>} />
-          <Route path='/games/:id' element={<div>Detail Game</div>} />
-          <Route path='/console/playstation' element={<div>Console:play</div>} />
-          <Route path='/console/xbox' element={<div>Console:xbox</div>} />
-          <Route path='/console/pc' element={<div>Console:pc</div>} />
-          <Route path='/console/switch' element={<div>Console:switch</div>} />
-          {/* <Route path='/order' element={<div>Orders</div>} /> */}
-          <Route path='/order/:id' element={<div>Detail Order</div>} />
-          <Route path='/mailing-list' element={<div>Mailing-list</div>} />
-          <Route path='*' element={<div>Error Page dont exist</div>} />
+          <Route element={<DefaultLayout />}>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/games' element={<div>Games</div>} />
+            <Route path='/games/:id' element={<div>Detail Game</div>} />
+            <Route path='/console/playstation' element={<div>Console:play</div>} />
+            <Route path='/console/xbox' element={<div>Console:xbox</div>} />
+            <Route path='/console/pc' element={<div>Console:pc</div>} />
+            <Route path='/console/switch' element={<div>Console:switch</div>} />
+            {/* <Route path='/order' element={<div>Orders</div>} /> */}
+            <Route path='/order/:id' element={<div>Detail Order</div>} />
+            <Route path='/mailing-list' element={<div>Mailing-list</div>} />
+            <Route path='*' element={<div>Error Page dont exist</div>} />
+          </Route >
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
