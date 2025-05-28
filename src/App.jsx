1e15fa143d0a2eb09da5fa0551d89b8629b8e997
AndreaRispoli97 <Andrea9715@outlook.it>
2025-05-28 08:58:15 +0200

    feat: implement routing and add HomePage page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 
 function App() {
 
   return (
     <>
-      <h1>Top Games!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/games' element={<div>Games</div>} />
+          <Route path='/games/:id' element={<div>Detail Game</div>} />
+          <Route path='/console/playstation' element={<div>Console:play</div>} />
+          <Route path='/console/xbox' element={<div>Console:xbox</div>} />
+          <Route path='/console/pc' element={<div>Console:pc</div>} />
+          <Route path='/console/switch' element={<div>Console:switch</div>} />
+          <Route path='/order' element={<div>Orders</div>} />
+          <Route path='/order/:id' element={<div>Detail Order</div>} />
+          <Route path='/mailing-list' element={<div>Mailing-list</div>} />
+          <Route path='*' element={<div>Error Page dont exist</div>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+
+
+function HomePage() {
+    console.log('Prova HomePage')
+
+    return (
+        <>
+            <h1>HOMEPAGE</h1>
+        </>
+    )
+}
+
+export default HomePage
