2a13c914da053bc76de3e29abdd9b9b088e73c8a
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-29 12:49:10 +0200

    feat: css layout search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -31,12 +31,12 @@ function SearchPage() {
 
         <>
             <section className="container">
-                <h2 className="text-center">Best Games</h2>
+                <h2 className="text-center text-white">Game Catalogue</h2>
 
                 <form onSubmit={searchGames} className="row g-1 justify-content-center ">
 
                     <div className="col-auto p-4">
-                        <input type="text" className="form-control" id="inputPassword2" placeholder="Search your games"
+                        <input type="text" className="form-control bg-warning" id="inputPassword2" placeholder="Search your games"
                             value={search} onChange={(e) => setSearch(e.target.value)}
                         />
 
@@ -45,13 +45,13 @@ function SearchPage() {
 
                 <div class="row row-gap-3 column-gap-2 d-flex justify-content-center">
                     {games.map(game =>
-                        <div className="card color-card col-4 w-25 " key={game.id}>
+                        <div className="card bg-dark col-4 w-25 text-white" key={game.id}>
                             <div className="card-body">
                                 <img src={game.imagePath} className="card-img-top " alt={game.title} />
                                 <h5 className="card-title fw-bold pt-2">{game.title}</h5>
                                 <p className="card-text">{game.description}</p>
                                 <p className="card-text fw-bold">{game.price} €</p>
-                                <button type="button" class="btn btn-primary">Product details</button>
+                                <button type="button" class="btn btn-warning fw-bold">Product details</button>
                             </div>
                         </div>
                     )}
