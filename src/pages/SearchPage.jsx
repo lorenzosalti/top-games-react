0bc313b34c90a191a84376ce5c03876fef27c361
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-05-29 18:57:10 +0200

    feat: fix layout card search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -37,14 +37,14 @@ function SearchPage() {
                     </form>
                 </div>
 
-                <div className="row row-gap-3 column-gap-2 d-flex justify-content-center">
+                <div className="row row-gap-3 mb-5 column-gap-2 d-flex justify-content-center">
                     {games.map(game =>
                         <div className="card bg-dark col-lg-3 col-md-4 text-white" key={game.id}>
                             <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                                <img src={game.imagePath} className="card-img-top " alt={game.title} />
+                                <figure className="mt-2"><img src={game.imagePath} className="card-img-top " alt={game.title} /></figure>
                                 <h5 className="card-title fw-bold pt-2">{game.title}</h5>
                                 <p className="card-text"><strong>Console:</strong> {game.platform}</p>
-                                <p className="card-text">{game.description}</p>
+                                <p className="card-text-description">{game.description}</p>
                                 <p className="card-text fw-bold">{game.price} €</p>
                                 <button type="button" className="btn btn-warning fw-bold">Dettaglio Prodotto</button>
                             </div>
