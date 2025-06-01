367723c718e904fcd989a9b0a9eb7dba93cbf727
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-06-01 12:11:15 +0200

    feat: upgrade layout cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import Search from './Search';
 import { useContext, useEffect } from 'react';
 import GlobalContext from '../contexts/globalContext';
@@ -7,8 +7,14 @@ import GlobalContext from '../contexts/globalContext';
 export default function Header() {
 
     const { cartStorage } = useContext(GlobalContext);
-    const totalPrice = cartStorage.reduce((acc, game) => acc + Number(game.price), 0)
+    const totalPrice = cartStorage.reduce((acc, game) => acc + Number(game.price), 0);
+    const navigate = useNavigate();
 
+    const handleCheckout = () => {
+        setTimeout(() => {
+            navigate('/checkout');
+        }, 100);
+    };
 
 
     return (
@@ -57,31 +63,46 @@ export default function Header() {
                     <h5 className="offcanvas-title" id="offcanvasRightLabel">Giochi nel carrello</h5>
                     <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-                <div className="offcanvas-body d-flex flex-column justify-content-between ">
+                <div className="offcanvas-body d-flex flex-column">
                     {cartStorage.length ? cartStorage.map((game, index) => (
                         <div key={index} className="card mb-3" >
-                            <div className="row g-0">
+                            <div className="row g-0 align-items-center">
                                 <div className="col-md-4">
                                     <img src={game.imagePath} className="img-fluid rounded-start" alt={game.title} />
                                 </div>
-                                <div className="col-md-8">
+                                <div className="col-md-6">
                                     <div className="card-body">
                                         <h5 className="card-title">{game.title}</h5>
-                                        <p className="card-text"><strong>Genere:</strong> {game.genres_list}</p>
-                                        <p className="card-text"><strong>Console:</strong> {game.platform}</p>
+                                        <p className="card-text mb-1"><strong>Genere:<br /></strong> {game.genres_list}</p>
+                                        <p className="card-text mb-2"><strong>Console:<br /></strong> {game.platform}</p>
                                         <p className="card-text fw-bold">Prezzo: {game.price} €</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2 text-end pe-4">
+                                    <div className="d-flex flex-column align-items-center">
+                                        <label htmlFor={`quantityInput${index}`} className="form-label">Quantità</label>
+                                        <input
+                                            type="number"
+                                            className="form-control mb-2"
+                                            id={`quantityInput${index}`}
+                                            min="1"
+                                            defaultValue="1"
+                                            style={{ width: '60px' }}
+                                        />
+                                        <button className="btn btn-danger btn-sm">
+                                            Elimina
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     ))
                         : <div>Nessun Gioco nel carrello</div>}
-                    {cartStorage.length ? <div className='align-self-end d-flex flex-column align-items-center'>
-                        <div className='mb-2'>Prezzo totale: {totalPrice.toFixed(2)}€</div>
-                        <Link to={'/checkout'} className="btn btn-primary" role="button">Sgancia i soldi</Link>
-                    </div> : ''}
-
                 </div>
+                {cartStorage.length ? <div className='d-flex flex-column align-items-end mb-4 me-4'>
+                    <div className='mb-2'>Prezzo totale: {totalPrice.toFixed(2)}€</div>
+                    <button className="btn btn-primary" onClick={handleCheckout} data-bs-dismiss="offcanvas">Sgancia i soldi</button>
+                </div> : ''}
             </div>
         </header>
     );
