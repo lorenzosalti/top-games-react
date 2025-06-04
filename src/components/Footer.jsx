3e57344c0055e1c87eaa32837e1c1d58bb1d02b7
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-06-04 12:09:28 +0200

    feat: add 4040 page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,7 +32,7 @@ const socialUrl = [
 export default function Footer() {
     return (
         <footer className='footer shadow'>
-            <div className='container-fluid bg-dark text-light py-4 mt-5'>
+            <div className='container-fluid bg-dark text-light py-4'>
                 <div className="footer-link-container">
                     <div>
                         <LinkFooter title="" links={GamesUrl} />
